15ac9b0fdc26833bb905ea340d14184ead4923ba
KONSTANTINOS <kostasfergadis74@gmail.com>
2023-07-25 15:20:46 +0300

    port env added

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ process.on(`unhandledRejection`, (reason, promise) => {
   process.exit(1);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
